43ecb6641b8873dfffbabcdabea580d95e1ba4d9
James Lim <james.lim.ori@gmail.com>
2020-07-19 03:16:06 +1000

    patched in bar height

--- config.def.h
+++ config.def.h
@@ -1,18 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 22;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const Bool viewontag         = False;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Fira Mono for Powerline:size=12" };
-static const char dmenufont[]       = "Fira Mono for Powerline:size=10";
+static const char *fonts[]          = { "FiraCode Nerd Font:size=12" };
+static const char dmenufont[]       = "FiraCode Nerd Font:size=10";
 static const char col_gray1[]       = "#231a18";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
